dfd8925eb9decd85507b4ce68bcf0c2888fa860d
ophx <69017612+ophx@users.noreply.github.com>
2025-05-13 16:22:38 -0400

    Delete javascript/commands/ping.js

--- javascript/commands/ping.js
+++
@@ -1,6 +0,0 @@
-export const name = "ping";
-export const aliases = ["p"]; // not required at all. you do not need this if you don't want it.
-
-export async function execute(msg, client, args) {
-    return await client.sendMsg(msg.flock_id, msg.nest_id, "Pong...");
-}
