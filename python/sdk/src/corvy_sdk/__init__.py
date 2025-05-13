b7e24a1ddd66d171d0cad4010dce0639458af93c
Whirling <infernity@infernity.dev>
2025-05-13 17:16:18 -0600

    add init lol

--- python/sdk/src/corvy_sdk/__init__.py
+++ python/sdk/src/corvy_sdk/__init__.py
@@ -2,5 +2,5 @@ from .messages import Message, MessageUser
 from .command_parsing import Greedy
 from .corvybot import CorvyBot
 
-__version__ = "1.8.0"
+__version__ = "1.8.1"
 __all__ = ["Greedy", "MessageUser", "Message", "CorvyBot"]
