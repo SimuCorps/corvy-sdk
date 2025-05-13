44d78ac59d18910ecc56eb9742d8cd592fa71c21
Whirling <infernity@infernity.dev>
2025-05-13 15:33:06 -0600

    i think the bugfix is worth a version bump to 1.8 to make sure people dont get 1.7 by mistake since its broken rn

--- python/sdk/src/corvy_sdk/__init__.py
+++ python/sdk/src/corvy_sdk/__init__.py
@@ -2,5 +2,5 @@ from .messages import Message, MessageUser
 from .command_parsing import Greedy
 from .corvybot import CorvyBot
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 __all__ = ["Greedy", "MessageUser", "Message", "CorvyBot"]
