ab21058308ea390a372feef42b61c57e34ba6c43
ophx <castytastyhappybat@gmail.com>
2025-05-14 11:56:45 -0400

    fixed?

--- javascript/docs/command-handler-example/commands/ping.js
+++ javascript/docs/command-handler-example/commands/ping.js
@@ -2,5 +2,5 @@ export const name = "ping";
 export const aliases = ["p", "pp"]; // not required if you don't want it
 
 export async function execute(msg, client, args) {
-    return "Pong!";
+  return "Pong!";
 }
--- javascript/docs/simple-bot.js
+++ javascript/docs/simple-bot.js
@@ -1,22 +1,22 @@
 import { Client } from "../sdk/corvy.js";
 
 const client = new Client({
-    token: "your_token",
-    prefix: ";", // default value
-    devMode: true // default value (true = more detailed logging)
+  token: "your_token",
+  prefix: ";", // default value
+  devMode: true, // default value (true = more detailed logging)
 });
 
 client.on("error", (err) => {
-    console.error(err);
+  console.error(err);
 });
 
 client.on("ready", (client) => {
-    console.log(`${client.user.name} is now online!`);
-    console.log(`I am in ${client.flocks.size} flocks.`);
+  console.log(`${client.user.name} is now online!`);
+  console.log(`I am in ${client.flocks.size} flocks.`);
 });
 
 client.registerCommand("ping", async (msg, client, args) => {
-    return "Pong!";
+  return "Pong!";
 });
 
 client.login();
