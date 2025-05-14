099bdf1659a5ab1172be7b480b432be513f207fd
Whirling <infernity@infernity.dev>
2025-05-14 16:21:49 -0600

    change version to 2.0

--- python/sdk/src/corvy_sdk/__init__.py
+++ python/sdk/src/corvy_sdk/__init__.py
@@ -3,5 +3,5 @@ from .command_parsing import Greedy
 from .corvybot import CorvyBot
 from .user import User
 
-__version__ = "1.8.3"
+__version__ = "2.0.0"
 __all__ = ["Greedy", "MessageUser", "Message", "CorvyBot", "User"]
