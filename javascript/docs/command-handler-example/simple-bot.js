b9b9c8c4280ae99b9bfeabe0ba32044f35fd74ba
ophx <castytastyhappybat@gmail.com>
2025-05-14 12:11:50 -0400

    revert

--- javascript/docs/command-handler-example/simple-bot.js
+++ javascript/docs/command-handler-example/simple-bot.js
@@ -3,26 +3,26 @@ import path from "path";
 import fs from "fs";
 
 const client = new Client({
-  token: "your_token",
-  prefix: ";", // default value
-  devMode: true, // default value (true = more detailed logging)
+    token: "your_token",
+    prefix: ";", // default value
+    devMode: true // default value (true = more detailed logging)
 });
 
 client.on("error", (err) => {
-  console.error(err);
+    console.error(err);
 });
 
 client.on("ready", (client) => {
-  console.log(`${client.user.name} is now online!`);
-  console.log(`I am in ${client.flocks.size} flocks.`);
+    console.log(`${client.user.name} is now online!`);
+    console.log(`I am in ${client.flocks.size} flocks.`);
 });
 
 const commandPath = path.resolve("./commands");
 const commandFiles = fs.readdirSync(commandPath).filter((file) => file.endsWith(".js"));
 for (let file of commandFiles) {
-  const command = await import(`./commands/${file}`);
-
-  client.registerCommand([command.name, ...(command.aliases ?? [])], command.execute);
+    const command = await import(`./commands/${file}`);
+    
+    client.registerCommand([command.name, ...(command.aliases ?? [])], command.execute);
 }
 
 client.login();
