e1633e19c64e0f9aecf695c00d46c2934066e332
Whirling <infernity@infernity.dev>
2025-05-13 22:47:57 -0600

    add User class

--- python/sdk/src/corvy_sdk/user.py
+++ python/sdk/src/corvy_sdk/user.py
@@ -11,4 +11,13 @@ class PartialUser:
         return self
     
     async def fetch(self) -> "User":
-        pass
+        async with self._connection_state.client_session.get(f"{self._connection_state.api_path}/users/{self.id}") as response:
+            data = await response.json()
+            return User(data["id"], data["username"], data["is_bot"], data["available_badges"], data.get("photo_url", None), data.get("badge", None))
+    
+@dataclass
+class User(PartialUser):
+    is_bot: bool
+    available_badges: list[str]
+    avatar_url: str | None
+    equipped_badge: str | None
