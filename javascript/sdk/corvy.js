cc0d1e34beff206a5cf93ddb15aa2d0dbc70b54d
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-05-14 01:05:25 -0500

    Simpler return Data
    Python API replies on return statements. This does the same

--- javascript/sdk/corvy.js
+++ javascript/sdk/corvy.js
@@ -45,7 +45,11 @@ export class Client extends EventEmitter {
       this.commands.set(fullPrefix.toLowerCase(), async (msg, client, argsText) => {
         try {
           this.log(`Executing command: ${fullPrefix}`);
-          await handler(msg, client, argsText);
+          this.log(`Executing command: ${fullPrefix}`);
+          const data = await handler(msg, client, argsText);
+          if (data) {
+            await client.sendMsg(msg.flock_id, msg.nest_id, data);
+          }
         } catch (err) {
           this.emit("commandError", fullPrefix, msg, err);
           this.log(`Error in command "${fullPrefix}": ${err.message}`);
