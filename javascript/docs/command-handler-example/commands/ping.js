245f8b6dc10248a7c2d5c2834af55dfea8d5906a
ophx <castytastyhappybat@gmail.com>
2025-05-14 11:15:06 -0400

    flock count & uptime. updated readme.

--- javascript/docs/command-handler-example/commands/ping.js
+++ javascript/docs/command-handler-example/commands/ping.js
@@ -2,5 +2,5 @@ export const name = "ping";
 export const aliases = ["p", "pp"]; // not required if you don't want it
 
 export async function execute(msg, client, args) {
-    return await client.sendMsg(msg.flock_id, msg.nest_id, "pong!");
+    return "Pong!";
 }
