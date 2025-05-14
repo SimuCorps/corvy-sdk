30ac16aba6dabe499c5979134da1cb2c2e106431
Whirling <infernity@infernity.dev>
2025-05-14 00:01:09 -0600

    user in top level

--- python/sdk/src/corvy_sdk/__init__.py
+++ python/sdk/src/corvy_sdk/__init__.py
@@ -1,6 +1,7 @@
 from .messages import Message, MessageUser
 from .command_parsing import Greedy
 from .corvybot import CorvyBot
+from .user import User
 
 __version__ = "1.8.3"
-__all__ = ["Greedy", "MessageUser", "Message", "CorvyBot"]
+__all__ = ["Greedy", "MessageUser", "Message", "CorvyBot", "User"]
