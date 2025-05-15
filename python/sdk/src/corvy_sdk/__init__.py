2ba0ae8e4a05c2cbdd440e1df22385af97f6efcd
Whirling <infernity@infernity.dev>
2025-05-14 21:57:57 -0600

    couple extra top level things

--- python/sdk/src/corvy_sdk/__init__.py
+++ python/sdk/src/corvy_sdk/__init__.py
@@ -2,6 +2,8 @@ from .messages import Message, MessageUser
 from .command_parsing import Greedy
 from .corvybot import CorvyBot
 from .user import User
+from .flock import Flock
+from .nest import Nest
 
 __version__ = "2.0.0"
-__all__ = ["Greedy", "MessageUser", "Message", "CorvyBot", "User"]
+__all__ = ["Greedy", "MessageUser", "Message", "CorvyBot", "User", "Flock", "Nest"]
